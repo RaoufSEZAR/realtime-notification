efcbe0acef78dcd2c1be333b5a689dfb06055109
raoufsezar <raoufsato999@gmail,com>
2022-06-12 23:43:25 +0300

    add notification to navbar component

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,38 +1,66 @@
-import React, { useEffect, useState } from "react";
-
 import "./navbar.css";
-import notif from "../../img/not.PNG";
-import settings from "../../img/settings.svg";
-import message from "../../img/message.svg";
+import Notification from "../../img/not.PNG";
+import Message from "../../img/message.svg";
+import Settings from "../../img/settings.svg";
+import { useEffect, useState } from "react";
 
-function Navbar({ socket }) {
+const Navbar = ({ socket }) => {
 	const [notifications, setNotifications] = useState([]);
+	const [open, setOpen] = useState(false);
+
 	useEffect(() => {
 		socket.on("getNotification", (data) => {
 			setNotifications((prev) => [...prev, data]);
 		});
 	}, [socket]);
 
-	console.log(notifications);
+	const displayNotification = ({ senderName, type }) => {
+		let action;
+
+		if (type === 1) {
+			action = "liked";
+		} else if (type === 2) {
+			action = "commented";
+		} else {
+			action = "shared";
+		}
+		return (
+			<span className="notification">{`${senderName} ${action} your post.`}</span>
+		);
+	};
+
+	const handleRead = () => {
+		setNotifications([]);
+		setOpen(false);
+	};
+
 	return (
 		<div className="navbar">
-			<span className="logo">Rauf SEZAR</span>
+			<span className="logo">Lama App</span>
 			<div className="icons">
-				<div className="icon">
-					<img src={notif} alt="" className="iconImg" />
-					<div className="counter">2</div>
+				<div className="icon" onClick={() => setOpen(!open)}>
+					<img src={Notification} className="iconImg radius" alt="" />
+					{notifications.length > 0 && (
+						<div className="counter">{notifications.length}</div>
+					)}
 				</div>
-				<div className="icon">
-					<img src={message} alt="" className="iconImg" />
-					<div className="counter">2</div>
+				<div className="icon" onClick={() => setOpen(!open)}>
+					<img src={Message} className="iconImg" alt="" />
 				</div>
-				<div className="icon">
-					<img src={settings} alt="" className="iconImg" />
-					<div className="counter">2</div>
+				<div className="icon" onClick={() => setOpen(!open)}>
+					<img src={Settings} className="iconImg" alt="" />
 				</div>
 			</div>
+			{open && (
+				<div className="notifications">
+					{notifications.map((n) => displayNotification(n))}
+					<button className="nButton" onClick={handleRead}>
+						Mark as read
+					</button>
+				</div>
+			)}
 		</div>
 	);
-}
+};
 
 export default Navbar;
