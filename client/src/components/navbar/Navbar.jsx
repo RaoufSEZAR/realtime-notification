79c926a9664a997203cb1b9a363551f172d147f9
raoufsezar <raoufsato999@gmail,com>
2022-06-12 21:45:05 +0300

    navbar style finished

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,8 +1,30 @@
-import "./navbar.css";
 import React from "react";
 
+import "./navbar.css";
+import notification from "../../img/not.PNG";
+import settings from "../../img/settings.svg";
+import message from "../../img/message.svg";
+
 function Navbar() {
-	return <div className="navbar">Navbar</div>;
+	return (
+		<div className="navbar">
+			<span className="logo">Rauf SEZAR</span>
+			<div className="icons">
+				<div className="icon">
+					<img src={notification} alt="" className="iconImg" />
+					<div className="counter">2</div>
+				</div>
+				<div className="icon">
+					<img src={message} alt="" className="iconImg" />
+					<div className="counter">2</div>
+				</div>
+				<div className="icon">
+					<img src={settings} alt="" className="iconImg" />
+					<div className="counter">2</div>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Navbar;
