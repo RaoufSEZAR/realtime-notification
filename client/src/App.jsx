38ba676dbdfcebb338f68a7000b133cab0968e91
raoufsezar <raoufsato999@gmail,com>
2022-06-12 22:39:32 +0300

    add socket to client side

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,46 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./app.css";
 import Card from "./components/card/Card";
 import Navbar from "./components/navbar/Navbar";
 import { posts } from "./data";
+import { io } from "socket.io-client";
 
-function App() {
+const App = () => {
 	const [username, setUsername] = useState("");
 	const [user, setUser] = useState("");
+	const [socket, setSocket] = useState(null);
+
+	useEffect(() => {
+		setSocket(io("http://localhost:5000"));
+	}, []);
+
+	useEffect(() => {
+		socket?.emit("newUser", user);
+	}, [socket, user]);
+
 	return (
 		<div className="container">
 			{user ? (
 				<>
-					<Navbar />
+					<Navbar socket={socket} />
 					{posts.map((post) => (
-						<Card post={post} key={post.id} />
+						<Card key={post.id} post={post} socket={socket} user={user} />
 					))}
 					<span className="username">{user}</span>
 				</>
 			) : (
 				<div className="login">
+					<h2>Lama App</h2>
 					<input
 						type="text"
 						placeholder="username"
 						onChange={(e) => setUsername(e.target.value)}
 					/>
-					<button onClick={() => setUser(username)}>login</button>
+					<button onClick={() => setUser(username)}>Login</button>
 				</div>
 			)}
 		</div>
 	);
-}
+};
 
 export default App;
