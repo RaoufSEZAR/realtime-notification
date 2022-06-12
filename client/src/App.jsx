6008b6efece4996d27f203c80bd9d023466f7dc5
raoufsezar <raoufsato999@gmail,com>
2022-06-12 13:29:27 +0300

    planing the comps

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,29 @@
-import "./App.css";
+import { useState } from "react";
+import "./app.css";
+import Card from "./components/card/Card";
+import Navbar from "./components/navbar/Navbar";
 
 function App() {
+	const [username, setUsername] = useState("");
+	const [user, setUser] = useState("");
 	return (
-		<div className="App">
-			<header className="App-header">
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-			</header>
+		<div className="container">
+			{user ? (
+				<>
+					<Navbar></Navbar>
+					<Card></Card>
+					<span className="username">{user}</span>
+				</>
+			) : (
+				<div className="login">
+					<input
+						type="text"
+						placeholder="username"
+						onChange={(e) => setUsername(e.target.value)}
+					/>
+					<button onClick={() => setUser(username)}>login</button>
+				</div>
+			)}
 		</div>
 	);
 }
